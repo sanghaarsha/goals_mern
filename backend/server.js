aaf4ca01ee79fcf4a1232da46d1ab299ef792b3c
Sangharsha Dahal <sanghaarsha@gmail.com>
2022-02-22 10:38:57 +0545

    add parsing and error handling middlewares

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middlewares/errorMiddleware");
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+// middlewares to parse body data and urlencoded
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/api/goals", require("./routes/goalRoutes"));
+
+// overwrite express default error handler
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Server live at http://localhost:${PORT}`);
