d40280fb658ab682c9ff63e7bea76c1438cdd78a
Sangharsha Dahal <sanghaarsha@gmail.com>
2022-02-22 13:32:42 +0545

    set-up user routes and controller

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/goals", require("./routes/goalRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 
 // overwrite express default error handler
 app.use(errorHandler);
