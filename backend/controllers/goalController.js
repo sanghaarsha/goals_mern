d4475b64ffe657de73266be3f4d745797b4b06e6
Sangharsha Dahal <sanghaarsha@gmail.com>
2022-02-22 12:44:04 +0545

    import goalModel, implement setting & getting goal

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,10 +1,12 @@
 const asyncHandler = require("express-async-handler");
+const Goal = require("../models/goalModel");
 
 // @desc Get Goals
 // @route GET /api/goals
 // @access Private
 const getGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: "Get Goals!" });
+    const goals = await Goal.find();
+    res.status(200).json(goals);
 });
 
 // @desc Set Goal
@@ -15,7 +17,10 @@ const setGoal = asyncHandler(async (req, res) => {
         res.status(400);
         throw new Error("Please add a text field!");
     }
-    res.status(200).json({ message: "Set Goal!" });
+    const goal = await Goal.create({
+        text: req.body.text,
+    });
+    res.status(200).json(goal);
 });
 
 // @desc Edit Goal
