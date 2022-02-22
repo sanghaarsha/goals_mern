d1decf9ca691123ebdac3e1f46c779fbcfd288bd
Sangharsha Dahal <sanghaarsha@gmail.com>
2022-02-22 10:21:16 +0545

    chaining routes

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,20 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const {
+    getGoals,
+    setGoal,
+    editGoal,
+    delGoal,
+} = require("../controllers/goalController");
 
-router.get("/", (req, res) => {
-    res.status(200).json({ message: "Get Goals!" });
-});
+// chaining routes
+// router.get("/", getGoals);
+// router.post("/", setGoal);
+router.route("/").get(getGoals).post(setGoal);
 
-router.post("/", (req, res) => {
-    res.status(200).json({ message: "Set Goals!" });
-});
-
-router.put("/:id", (req, res) => {
-    res.status(200).json({ message: `Update goal: ${req.params.id}` });
-});
-
-router.delete("/:id", (req, res) => {
-    res.status(200).json({ message: `Delete goal: ${req.params.id}` });
-});
+// router.put("/:id", editGoal);
+// router.delete("/:id", delGoal);
+router.route("/:id").put(editGoal).delete(delGoal);
 
 module.exports = router;
