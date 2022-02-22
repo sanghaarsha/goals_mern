bdfd716172a420ee5325747ebd32b863601ec452
Sangharsha Dahal <sanghaarsha@gmail.com>
2022-02-22 13:18:47 +0545

    associate each goal to a user

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -2,6 +2,12 @@ const mongoose = require("mongoose");
 
 const goalSchema = mongoose.Schema(
     {
+        // associating user to the goal
+        user: {
+            type: mongoose.Schema.Types.ObjectId, //_id type
+            required: true,
+            ref: "User", // reference to User model
+        },
         text: {
             type: String,
             required: [true, "Please add a text value"],
