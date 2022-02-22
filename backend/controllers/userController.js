6aba3a3b9982479478e0f62d43dbe95a9119b76e
Sangharsha Dahal <sanghaarsha@gmail.com>
2022-02-22 13:41:41 +0545

    import packages & wrap controllers w. asyncHandler

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,23 +1,28 @@
+const jwt = import("jsonwebtoken");
+const bcrypt = import("bcryptjs");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
 // @desc Register new user
 // @route POST /api/users/
 // @access Public
-const registerUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
     res.status(200).json({ message: "Register User" });
-};
+});
 
 // @desc Authenticate a user
 // @route POST /api/users/login
 // @access Public
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
     res.status(200).json({ message: "Login User" });
-};
+});
 
 // @desc Get user data
 // @route GET /api/users/me
 // @access Private
-const getMe = (req, res) => {
+const getMe = asyncHandler(async (req, res) => {
     res.status(200).json({ message: "User Data" });
-};
+});
 
 module.exports = {
     registerUser,
