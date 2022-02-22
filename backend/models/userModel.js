755d5912458122441878e16edb389b2dd4d8fb2d
Sangharsha Dahal <sanghaarsha@gmail.com>
2022-02-22 13:44:30 +0545

    minor error fix

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,20 +1,22 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, "Please add a name"],
+const userSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Please add a name"],
+        },
+        email: {
+            type: String,
+            required: [true, "Please add an E-mail"],
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: [true, "Please add a password"],
+        },
     },
-    email: {
-        type: String,
-        required: [true, "Please add an E-mail"],
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: [true, "Please add a password"],
-    },
-    timestamps: true,
-});
+    { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
