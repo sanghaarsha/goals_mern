8a4e42d2299a284c4bff0dcce13db91b7fdf62b4
Sangharsha Dahal <sanghaarsha@gmail.com>
2022-02-22 15:23:21 +0545

    protect goal routes

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -6,14 +6,15 @@ const {
     editGoal,
     delGoal,
 } = require("../controllers/goalController");
+const { protect } = require("../middlewares/authMiddleware");
 
 // chaining routes
 // router.get("/", getGoals);
 // router.post("/", setGoal);
-router.route("/").get(getGoals).post(setGoal);
+router.route("/").get(protect, getGoals).post(protect, setGoal);
 
 // router.put("/:id", editGoal);
 // router.delete("/:id", delGoal);
-router.route("/:id").put(editGoal).delete(delGoal);
+router.route("/:id").put(protect, editGoal).delete(protect, delGoal);
 
 module.exports = router;
