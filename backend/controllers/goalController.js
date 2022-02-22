52e2eebbee1ed16e61520eaf3e873e98e3ee2f1c
Sangharsha Dahal <sanghaarsha@gmail.com>
2022-02-22 12:56:01 +0545

    implement updating & removing goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -27,14 +27,32 @@ const setGoal = asyncHandler(async (req, res) => {
 // @route PUT /api/goals/:id
 // @access Private
 const editGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Update goal: ${req.params.id}` });
+    const goal = await Goal.findById(req.params.id);
+
+    if (!goal) {
+        res.status(400);
+        throw new Error("Goal not found!");
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(
+        req.params.id,
+        req.body.text,
+        { new: true } // create new goal if it does not exist
+    );
+    res.status(200).json(updatedGoal);
 });
 
 // @desc Delete Goal
 // @route DELETE /api/goals/:id
 // @access Private
 const delGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Delete goal: ${req.params.id}` });
+    const goal = await Goal.findById(req.params.id);
+    if (!goal) {
+        res.status(400);
+        throw new Error("Goal not found!");
+    }
+    await Goal.findByIdAndRemove(req.params.id);
+    res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
