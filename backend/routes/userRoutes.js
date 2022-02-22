df1774c611675b8ae061cb1e827806ac6d51129f
Sangharsha Dahal <sanghaarsha@gmail.com>
2022-02-22 15:12:08 +0545

    protect 'me' route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,11 +4,12 @@ const {
     loginUser,
     getMe,
 } = require("../controllers/userController");
+const { protect } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
 router.post("/", registerUser);
 router.post("/login", loginUser);
-router.get("/me", getMe);
+router.get("/me", protect, getMe);
 
 module.exports = router;
